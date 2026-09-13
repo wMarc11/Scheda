f29273b3cf5eec70c31992bdb71e411f0f304899
Marc <marcadrian.bobadilla@gmail.com>
2026-09-13 13:04:53 +0800

    feat: add color coding to timetable events

--- src/components/schedule/TimetableEvent.jsx
+++ src/components/schedule/TimetableEvent.jsx
@@ -12,10 +12,20 @@ const TimetableEvent = ({ section, schedule }) => {
 
     const top = ((startMinutes - timeTableStart) / 60) * pixelsPerHour;
     const height = (duration / 60) * pixelsPerHour;
-    
+
+    const firstTwoLettersInSection = section.courseId.slice(0, 2);
+
+    const color = firstTwoLettersInSection === "CC" ?
+        "bg-blue-100 text-blue-900" : 
+        firstTwoLettersInSection === "GE" ?
+        "bg-green-100 text-green-900" :
+        firstTwoLettersInSection === "CS" ?
+        "bg-amber-100 text-amber-900" :
+        "bg-blue-100 text-blue-900";
+
     return(
         <div
-            className="absolute rounded-md bg-blue-100 p-1 lg:p-2 text-[clamp(6px,0.7vw,10px)] leading-tight text-blue-900 text-center flex flex-col justify-center mx-auto overflow-hidden"
+            className={`absolute rounded-md ${color} p-1 lg:p-2 text-[clamp(6px,0.7vw,10px)] leading-tight text-center flex flex-col justify-center mx-auto overflow-hidden`}
             style={{
                 top: `${top}px`,
                 height: `${height}px`,
