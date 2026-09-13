375fd84126047d5aea4005380fba054c2bac8c7c
Marc <marcadrian.bobadilla@gmail.com>
2026-09-13 14:04:06 +0800

    feat: add empty course search state and Google Icon Search Symbol

--- src/components/courses/CourseList.jsx
+++ src/components/courses/CourseList.jsx
@@ -2,6 +2,7 @@ import CourseCard from "./CourseCard";
 
 const CourseList = ({ courses }) => {
     return (
+        courses.length > 0 ?
         <div>
             {courses.map((course) => {
                 return(
@@ -11,6 +12,13 @@ const CourseList = ({ courses }) => {
                     />
                 );
             })}
+        </div> :
+        <div className="flex flex-col p-4 justify-center items-center mt-31">
+            <span className="material-symbols-outlined text-gray-400 !text-[36px]">
+                search
+            </span>
+            <h1 className="font-semibold">No courses found.</h1>
+            <p className="font-gray-500 ">Try another search term or clear your filters.</p>
         </div>
     );
 };
